bf73521cff04f0e445be93ce24c1337ffe1062c0
Chinseok Lee <me@nomade.kr>
2018-01-31 13:39:08 +0900

    dojo1앱에 Field 모델 생성 및 등록

--- networkons/dojo1/models.py
+++ networkons/dojo1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Field(models.Model):
+    name = models.CharField(max_length=100)
+    # position = models.PointField()
+    latlng = models.CharField(max_length=50,
+            help_text='"위도,경도" 포맷으로 입력해주세요.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
