dde5df88067aaf1e18d19c615b804aee1d32647d
Chinseok Lee <me@nomade.kr>
2018-01-31 12:27:05 +0900

    author 필드 추가 및 admin 등록

--- networkons/blog/models.py
+++ networkons/blog/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+# class ZipCode(models.Model):
+#     code = models.CharField(max_length=5, unique=True)
 
