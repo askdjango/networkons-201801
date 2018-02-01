cf79571b5eaa4d4d7733e5c513cc46f7094bf748
Chinseok Lee <me@nomade.kr>
2018-02-01 11:00:58 +0900

    Admin 커스텀

--- networkons/dojo1/admin.py
+++ networkons/dojo1/admin.py
@@ -10,6 +10,19 @@ admin.site.register(Customer, CustomerAdmin)
 
 class FieldAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'latlng', 'status']
+    list_display_links = ['name']
+    list_editable = ['latlng']
+    list_filter = ['status']
+    actions = ['make_open', 'make_close']
+
+    def make_open(self, request, queryset):
+        count = queryset.update(status='open')
+        self.message_user(request, '{}건을 열림상태로 변경했습니다.'.format(count))
+    make_open.short_description = '열림 상태로 변경'
+
+    def make_close(self, request, queryset):
+        count = queryset.update(status='close')
+        self.message_user(request, '{}건을 닫힘상태로 변경했습니다.'.format(count))
 
 admin.site.register(Field, FieldAdmin)
 
